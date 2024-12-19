8942b8eedf1220ffaf01cc28882420e547bec226
Anton Koluh <antonkoluh1@gmail.com>
2024-12-19 18:16:49 +0200

    added HeadnLegsNum.py

---
+++ HeadNLegsNum.py
@@ -0,0 +1,12 @@
+heads = int(input("Enter Heads: "))
+legs = int(input("Enter Legs: "))
+
+parts = (heads, legs)
+cows = int(parts[1]/2 - parts[0])
+results = (abs(cows), abs(parts[0] - cows))
+check_parts = ((results[0] + results[1]), (results[0]*4 + results[1]*2))
+
+if parts == check_parts:
+    print(f"(Cow, Chicken): {results}")
+else:
+    print ("Impossible")
