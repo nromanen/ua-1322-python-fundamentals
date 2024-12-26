4f0660653e01f7ae16e817006ecd5672ce8fbefc
Ihor Bohush <boigor95@gmail.com>
2024-12-27 00:57:10 +0200

    delete files

--- HW1/01-1_Practical_Task1.py
+++
@@ -1,9 +0,0 @@
-# Блок запитань до користувача
-first_name = input('What is your name? ')
-age = input('How old are you? ')
-city = input('Where do you live? ')
-
-# Блок повідомлень користувачу
-print('Hello,', first_name)
-print('Your age is', age)
-print('You live in', city)
