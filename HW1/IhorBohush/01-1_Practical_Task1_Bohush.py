9e351737050efebc776f1aa77c06813684ef90d5
Ihor Bohush <boigor95@gmail.com>
2024-12-27 00:52:25 +0200

    create folder

---
+++ HW1/IhorBohush/01-1_Practical_Task1_Bohush.py
@@ -0,0 +1,9 @@
+# Блок запитань до користувача
+first_name = input('What is your name? ')
+age = input('How old are you? ')
+city = input('Where do you live? ')
+
+# Блок повідомлень користувачу
+print('Hello,', first_name)
+print('Your age is', age)
+print('You live in', city)
