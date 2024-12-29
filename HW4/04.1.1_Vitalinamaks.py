0021e2d5d6c1edf380b0cc7b460eae375a0d5b5c
Vita Maksymchenko <vitamaksymchenko@MacBook-Air-Vita.local>
2024-12-29 16:26:49 +0200

    add 4.1 task

---
+++ HW4/04.1.1_Vitalinamaks.py
@@ -0,0 +1,7 @@
+temp_cel=float(input("Enter the temperature in Celsius"))
+
+if temp_cel<-273.15:
+    print(f"Temperature below absolute zero(-273.15°C)")
+else:
+    temp_fahr=(temp_cel * 9/5) + 32
+    print(f"{temp_cel}°C is equivalent to {temp_fahr}°F")
