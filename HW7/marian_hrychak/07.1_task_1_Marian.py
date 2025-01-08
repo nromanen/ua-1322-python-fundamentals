28e4694a7e7be0ece089f4edff4d43f6bae33eee
Marian <mv.hrychak91@gmail.com>
2025-01-09 00:08:16 +0200

    add HW7 task1

---
+++ HW7/marian_hrychak/07.1_task_1_Marian.py
@@ -0,0 +1,11 @@
+"""
+Write a function that returns the largest number of two
+numbers (use DocStrings documentation strings in the function).
+"""
+
+def the_largest_number(num1, num2):
+    """The function takes two numbers and returns the largest of them."""
+    if num1 > num2:
+        return num1
+    else:
+        return num2
