cba6be7f917df2c17d7eff145ca7f38ef1e7a812
Olena Budko <elen199@ukr.net>
2025-01-12 11:59:11 +0200

    typo fix

--- HW7/OlenaBudko/Task_7_3.py
+++ HW7/OlenaBudko/Task_7_3.py
@@ -19,7 +19,6 @@ def filter_words(st):
     while st1.find("  ") != -1:
         st1 = st1.replace("  ", " ")
     return st1.strip()
-a = "teyryb     RTDFTYGVG hdbf hhdTT"
 
 # 4. We need a function that can transform a number (integer) into a string.
 # What ways of achieving this do you know?
--- HW8/OlenaBudko/Task_8_2.py
+++ HW8/OlenaBudko/Task_8_2.py
@@ -6,10 +6,10 @@ def validate(password_user):
     pat_num = "[\d]"
     pat_spec = "[$#@]"
     if not re.findall(pat_letter, password_user):
-        print("Password does not have 1 letter between [a-x,A-Z]")
+        print("Password does not have 1 letter between [a-z,A-Z]")
         return False
     if not re.findall(pat_num, password_user):
-        print("Password does not have 1 number between [0:9]")
+        print("Password does not have 1 number between [0-9]")
         return False
     if not re.findall(pat_spec, password_user):
         print("Password does not have 1 character from [\$#@]")
