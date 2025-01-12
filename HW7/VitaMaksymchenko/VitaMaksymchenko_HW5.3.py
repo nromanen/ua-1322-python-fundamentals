8efc70734e997760cda2fa1a0b15b5dc920a6d91
Vita Maksymchenko <vitamaksymchenko@MacBook-Air-Vita.local>
2025-01-12 16:14:30 +0200

    add HW7

--- HW7/VitaMaksymchenko/VitaMaksymchenko_HW5.3.py
+++ HW7/VitaMaksymchenko/VitaMaksymchenko_HW5.3.py
@@ -1,8 +1,8 @@
 word = input("Enter the word: ")
 number = {}
 for letter in word:
-    if letter not in number: #check each letter and if it was not previously in the set, assign it a value =1 
+    if letter not in number: #check each letter and if it was not previously in the dict, assign it a value =1 
         number[letter] = 1
     else:
-        number[letter] += 1 #if this letter was in the set, count the number
+        number[letter] += 1 #if this letter was in the dict, count the number
 print(number)
