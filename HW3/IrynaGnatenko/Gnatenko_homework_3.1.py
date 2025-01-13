1a6185b72f4183b4f9e3d7813aa5568009d902ac
Iryna Gnatenko <irynagnatenko@gmail.com>
2025-01-14 00:04:14 +0100

    Added HW4

--- HW3/IrynaGnatenko/Gnatenko_homework_3.1.py
+++ HW3/IrynaGnatenko/Gnatenko_homework_3.1.py
@@ -7,24 +7,24 @@ You need to write Python's philosophy in the string format:
 """
 
 pythonPhilosophy = str('''Beautiful is better than ugly.
-    Explicit is better than implicit.
-    Simple is better than complex.
-    Complex is better than complicated.
-    Flat is better than nested.
-    Sparse is better than dense.
-    Readability counts.
-    Special cases aren't special enough to break the rules.
-    Although practicality beats purity.
-    Errors should never pass silently.
-    Unless explicitly silenced.
-    In the face of ambiguity, refuse the temptation to guess.
-    There should be one-- and preferably only one --obvious way to do it.
-    Although that way may not be obvious at first unless you're Dutch.
-    Now is better than never.
-    Although never is often better than *right* now.
-    If the implementation is hard to explain, it's a bad idea.
-    If the implementation is easy to explain, it may be a good idea.
-    Namespaces are one honking great idea -- let's do more of those!''')
+	Explicit is better than implicit.
+	Simple is better than complex.
+	Complex is better than complicated.
+	Flat is better than nested.
+	Sparse is better than dense.
+	Readability counts.
+	Special cases aren't special enough to break the rules.
+	Although practicality beats purity.
+	Errors should never pass silently.
+	Unless explicitly silenced.
+	In the face of ambiguity, refuse the temptation to guess.
+	There should be one-- and preferably only one --obvious way to do it.
+	Although that way may not be obvious at first unless you're Dutch.
+	Now is better than never.
+	Although never is often better than *right* now.
+	If the implementation is hard to explain, it's a bad idea.
+	If the implementation is easy to explain, it may be a good idea.
+	Namespaces are one honking great idea -- let's do more of those!''')
 
 
 """
@@ -32,52 +32,22 @@ find separately the number of occurrences of the words \
 "better", "never" and "is" in the given line
 """
 
-# split the multiline string into separate lines:
-lines = pythonPhilosophy.splitlines()
+words = ['better','never','is']
+text = pythonPhilosophy
 
-# specify which words we want to count:
-wordsCounted = ['better','never','is']
+def word_count(text,words):
+	""" a function to count each of the words in the given text"""
+	for currentWord in words:
+		wordQuantity: int = text.count(currentWord)
+		print(f'{currentWord}:{wordQuantity}')
+		print()
 
-# create the variables to contain the line and word we will be working on, \
-# assign the first value:
-currentLine = str(lines[0])
-currentWord = str(wordsCounted[0])
+word_count(text,words)
 
-# create a function to count each of the words in the current line:
-def word_count():
-
-    for currentWord in wordsCounted:
-        wordQuantity: int = currentLine.count(currentWord)
-        print(f'{currentWord}:{wordQuantity}')
-
-# print each line and count the words in it, print the result, \
-# and add an empty line for better readability:
-for currentLine in lines:
-    print(f'{currentLine.strip()}')
-    word_count()
-    print ()
-
-"""
-you need to display all text in uppercase (all letters in uppercase)
-"""
-
-pythonPhilosophy=pythonPhilosophy.upper()
-lines = pythonPhilosophy.splitlines()
-currentLine = str(lines[0])
-
-for currentLine in lines:
-    print(f'{currentLine.strip()}')
-
+""" you need to display all text in uppercase (all letters in uppercase) """
+print (pythonPhilosophy.upper())
 print()
 
-"""
-replace all occurrences of the symbol “i” with “&”
-"""
-
-pythonPhilosophy=pythonPhilosophy.replace('I','&')
-
-lines = pythonPhilosophy.splitlines()
-currentLine = str(lines[0])
-
-for currentLine in lines:
-    print(f'{currentLine.strip()}')
+""" replace all occurrences of the symbol “i” with “&” """
+print (pythonPhilosophy.replace('I','&'))
+print()
