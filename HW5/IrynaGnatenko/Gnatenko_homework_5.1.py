44b8fa3c7a48ba43e49d9ed60f0de4f1b0df765b
Iryna Gnatenko <irynagnatenko@gmail.com>
2025-01-14 22:54:15 +0100

    Added HW5.1 - another solution

--- HW5/IrynaGnatenko/Gnatenko_homework_5.1.py
+++ HW5/IrynaGnatenko/Gnatenko_homework_5.1.py
@@ -4,11 +4,20 @@ the loop to change the type of these elements to a floating type.
 (Hint: use the built-in float () function).
 """
 
-integer_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]      # Create a list of integers
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]      # Create a list of integers
 float_list = []                                     # Create a list of floats
 
-for x in integer_list:                              # Use a loop to convert each element to a floating-point number
+for x in list:                              # Use a loop to convert each element to a floating-point number
 	float_list.append(float(x))
 
-print(f"integer list: {integer_list}")              # Print the list of integers
+print(f"integer list: {list}")              # Print the list of integers
 print(f"float list: {float_list}")                  # Print the list of floats.
+print ()
+
+# Alternatively, we can use range(len(sequence)) for iterating over the list:
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in range(len(list)):
+	list[i] = float(list[i])
+
+print (list)
