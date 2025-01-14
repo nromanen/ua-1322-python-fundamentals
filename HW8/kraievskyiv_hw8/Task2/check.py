bdd3483cea0c90a709b6cd10f8e2b4ab15c4b2cf
kraievskyiv <136990411+kraievskyiv@users.noreply.github.com>
2025-01-14 23:13:31 +0200

    replaced common functions with re, made it in one file

--- HW8/kraievskyiv_hw8/Task2/check.py
+++ HW8/kraievskyiv_hw8/Task2/check.py
@@ -1,20 +1,45 @@
-import functions as f
+import re
+
+def is_lower(i):
+    if re.search(r"[a-z]", i):
+        return True 
+    return False
+
+def is_upper(i):
+    if re.search(r"[A-Z]", i):
+        return True
+    return False
+
+def is_number(i):
+    if re.search(r"\d", i):
+        return True
+    return False
+
+def is_special(i):
+    if re.search(r"[@#$]", i):
+        return True
+    return False
+
+def right_length(i):
+    if 6 <= len(i) <= 16:
+        return True
+    return False
 
 def check_password():
     errors = []
     while True:
         password = input("Enter your password: ")
-        if not f.is_lower(password):
+        if not is_lower(password):
             errors.append("Password must contain at least one lowercase letter.")
-        if not f.is_upper(password):
+        if not is_upper(password):
             errors.append("Password must contain at least one uppercase letter.")
-        if not f.is_number(password):
+        if not is_number(password):
             errors.append("Password must contain at least one number.")
-        if not f.right_length(password):
+        if not right_length(password):
             errors.append("Password must contain minimum 6 and maximum 16 characters.")
-        if not f.is_special(password):
+        if not is_special(password):
             errors.append("Password must contain at least one of the following characters: $, #, @.")
-    
+
         if errors:
             for error in errors:
                 print(error)
@@ -23,5 +48,4 @@ def check_password():
             print("Welcome!")
             break
 
-if __name__ == '__main__':
-    check_password()
+check_password()
