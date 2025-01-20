781c8f30b9a89ec27cf66cfcaecbce674877cd5a
Anton Koluh <antonkoluh1@gmail.com>
2025-01-20 14:46:09 +0200

    fix loop 11 task1

--- HW11/KoluhAnton/11-01-task1.py
+++ HW11/KoluhAnton/11-01-task1.py
@@ -2,6 +2,8 @@ from check_func import check
 
 
 if __name__ == '__main__':
-    age = input("Enter your age: ")
-    if check(age):
-        print ("Odd" if int(age)%2 else "Even")
+    while True:
+        age = input("Enter your age: ")
+        if check(age):
+            print ("Odd" if int(age)%2 else "Even")
+            break
