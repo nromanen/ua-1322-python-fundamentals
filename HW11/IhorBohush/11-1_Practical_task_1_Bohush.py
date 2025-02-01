f573124d3f7d99a0e62ddb5f942cb8aac8624fb7
IhorBohush <boigor95@gmail.com>
2025-02-01 17:53:39 +0200

    change 11-1_Practical_task_1_Bohush.py

--- HW11/IhorBohush/11-1_Practical_task_1_Bohush.py
+++ HW11/IhorBohush/11-1_Practical_task_1_Bohush.py
@@ -1,16 +1,14 @@
 def age_status(age):
     """This function processes the information to check is the user's age even or odd number.
     If a number of the user's age is negative it raises an exception"""
-    try:
-        age = int(age)
-        if age <= 0:
-            raise ValueError(f'that is not a positive number: {age}')
-        elif age % 2 == 0:
-            return 'Your age is an even number'
-        else:
-            return 'Your age is an odd number'
-    except ValueError as e:
-        return e
+
+    age = int(age)
+    if age <= 0:
+        return ValueError(f'that is not a positive number: {age}')
+    elif age % 2 == 0:
+        return 'Your age is an even number'
+    else:
+        return 'Your age is an odd number'
 
 
 # It's a master code that prompts the users to enter their age and calls a function that
