ab0fa7c04b9ccf39e665ccfcf2762fbe2f658890
yholovii <yulia.goloviy@gmail.com>
2025-02-08 21:15:58 +0200

    Added task 07.1 practical task 1

---
+++ HW7/07.1_practical task 1_Yuliia Holovii.py
@@ -0,0 +1,14 @@
+def get_largest_number(a, b):
+    """
+    Returns the largest of two numbers.
+    
+    Parameters:
+    a (float): The first number
+    b (float): The second number
+    
+    Returns:
+    float: The largest number
+    """
+    return max(a, b)
+
+print(get_largest_number(10, 20))
